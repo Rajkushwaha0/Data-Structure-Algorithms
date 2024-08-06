f55d91f8e49656453e328928f6935dfe515ad2d7
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2024-08-06 19:02:32 +0530

    Update LC-M-3016-Minimum Number of Pushes to Type Word II.java

--- Learn-Hashing/LC-M-3016-Minimum Number of Pushes to Type Word II.java
+++ Learn-Hashing/LC-M-3016-Minimum Number of Pushes to Type Word II.java
@@ -1,6 +1,31 @@
+TC -> O(n)
+SC -> O(1)
+class Solution {
+    public int minimumPushes(String word) {
+        
+        int[] letterFrequency = new int[26];
+        for (char c : word.toCharArray()) {
+            letterFrequency[c - 'a']++;
+        }
+        
+        Integer[] sortedFreq = new Integer[26];
+        for (int i = 0; i < 26; i++) {
+            sortedFreq[i] = letterFrequency[i];
+        }
+        Arrays.sort(sortedFreq, Collections.reverseOrder());
+        int totalPresses = 0;
+        for (int i = 0; i < 26; i++) {
+            if (sortedFreq[i] == 0) break;
+            totalPresses += (i / 8 + 1) * sortedFreq[i];
+        }
+        
+        return totalPresses;
+
+    }
+}
+
 TC -> O(n*log n)
 SC -> O(n)
-
 class Solution {
     public int minimumPushes(String word) {
         HashMap<Character,Integer> hm = new HashMap<>();
