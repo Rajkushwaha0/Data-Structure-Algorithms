bac53e6385132b925e8c6dbbbde873c78a62d053
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2024-08-16 02:23:10 +0530

    Update LL-LC-H-25-Reverse Nodes in k-Group.java

--- Learn_Hard_Problem/LL-LC-H-25-Reverse Nodes in k-Group.java
+++ Learn_Hard_Problem/LL-LC-H-25-Reverse Nodes in k-Group.java
@@ -1,3 +1,59 @@
+//Optimize 
+class Solution {
+    //O(n)+O(n) -> O(2n)
+    //O(1)
+    public ListNode findKthNode(ListNode head,int k){
+        k-=1;
+        while(head!=null && k>0){
+            k--;
+            head=head.next;
+        }
+        return head;
+    }
+    public ListNode reverse(ListNode head){
+        if(head==null || head.next == null) return head;
+
+        ListNode newNode = reverse(head.next);
+        head.next.next=head;
+        head.next=null;
+        return newNode;
+    }
+    public ListNode reverseKGroup(ListNode head, int k) {
+        if(head == null || head.next == null) return head;
+
+        ListNode temp = head;
+        ListNode prevNode = null;
+        ListNode nextNode = null;
+        while(temp!=null){
+            ListNode kthnode = findKthNode(temp,k); //O(n)
+            if(kthnode == null ){
+                if(prevNode!=null){
+                    prevNode.next=temp;
+                }
+                break;
+            }
+            else{
+                nextNode = kthnode.next;
+                kthnode.next=null;
+                reverse(temp);//O(n)
+                if(temp==head){
+                    head=kthnode;
+                }
+                else{
+                    prevNode.next=kthnode;
+                }
+                prevNode=temp;
+                temp = nextNode;
+            }
+        }
+        return head;
+    }
+}
+
+
+
+
+
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         int tNode=totalNode(head);
