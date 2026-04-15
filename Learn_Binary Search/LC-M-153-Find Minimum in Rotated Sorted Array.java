37a5db89a638b564ae12930659c8cd6cd903a59d
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2026-04-16 01:53:44 +0530

    Update LC-M-153-Find Minimum in Rotated Sorted Array.java

--- Learn_Binary Search/LC-M-153-Find Minimum in Rotated Sorted Array.java
+++ Learn_Binary Search/LC-M-153-Find Minimum in Rotated Sorted Array.java
@@ -2,21 +2,29 @@
 //O(1)
 class Solution {
     public int findMin(int[] nums) {
-        int n=nums.length;
-        int s=0;
-        int e=n-1;
-        int ans=Integer.MAX_VALUE;
-        if(n==1) return nums[0];
-        while(s<=e){
-            int mid = s+(e-s)/2;
-            if(nums[s]<=nums[mid]){
-                ans=Math.min(ans,nums[s]);
-                s=mid+1;
-            }else{
-                ans=Math.min(ans,nums[mid]);
-                e=mid-1;
+        int n = nums.length;
+        int s = 0;
+        int e = n-1;
+
+        int ans = Integer.MAX_VALUE;
+
+        while( s<=e ){
+            int mid = s + (e-s)/2;
+
+            if( nums[mid] <= nums[e] ){
+                ans = Math.min(ans,nums[mid]);
+                e = mid-1;
             }
+
+            if ( nums[s] <= nums[mid] ){
+                ans = Math.min(ans,nums[s]);
+                s = mid+1; 
+            }
+
         }
+
         return ans;
     }
+
+    //logic simple hai go for sorted array that has minimum value
 }
