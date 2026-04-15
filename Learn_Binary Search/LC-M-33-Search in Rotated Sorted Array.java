8e235d78549c52ccbf9dcecc0e179faf94571597
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2026-04-16 01:27:35 +0530

    Create LC-M-33-Search in Rotated Sorted Array.java

---
+++ Learn_Binary Search/LC-M-33-Search in Rotated Sorted Array.java
@@ -0,0 +1,36 @@
+//TC: O(log(n))
+//SC O(1)
+class Solution {
+    public int search(int[] nums, int target) {
+        int n = nums.length;
+        int s = 0;
+        int e = n-1;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            if(nums[mid]==target) return mid;
+
+            if( nums[s] <= nums[mid]){
+                if(target>=nums[s] && target<=nums[mid]){
+                    e=mid-1;
+                }else{
+                    s=mid+1;
+                }
+            }
+
+            if( nums[mid] <= nums[e] ){
+                if(target>=nums[mid] && target<=nums[e]){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
+            }
+
+            
+
+        }
+
+        return -1;
+    }
+}
