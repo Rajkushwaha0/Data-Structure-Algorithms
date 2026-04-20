3570095f5b75ace1ecdd2825f5b146a409d8d220
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2026-04-20 12:28:55 +0530

    Update LC-M-1011-Capacity To Ship Packages Within D Days.java

--- Learn_Binary Search/LC-M-1011-Capacity To Ship Packages Within D Days.java
+++ Learn_Binary Search/LC-M-1011-Capacity To Ship Packages Within D Days.java
@@ -2,35 +2,52 @@
 //O(1)
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-        int n = weights.length;
-        int s = 1;
-        int e = 0;
-        for(int i=0;i<n;i++){
-            e+=weights[i];
-            s = Math.max(s, weights[i]);
-        }
+        int[] sumMinArray = sumOfArray(weights);
+        int s = sumMinArray[0];
+        int e = sumMinArray[1];
+
         while(s<=e){
-            int mid = (s+e)/2;
-            boolean minDays = maxLimit(weights,mid,days,n);
-            if(minDays){
+            int mid = s + (e-s)/2;
+
+            if( weightLoadedforShipment(weights,days,mid) ){
                 e = mid-1;
             }else{
                 s = mid+1;
             }
         }
+
         return s;
     }
-    public boolean maxLimit(int[] weights,int sum,int days, int n){
-        int rangeSum =0;
-        int daysCount=1;
-        for(int i=0;i<n;i++){
-            if((rangeSum+weights[i])>sum){
-                daysCount+=1;
-                rangeSum = weights[i];
+
+    public boolean weightLoadedforShipment(int[] weights,int days, int mid){
+        int totalDaysUsed = 1;
+        int sum = 0;
+        System.out.println(" ");
+        for(int i : weights){
+            
+            if((sum+i)>mid){
+                totalDaysUsed+=1;
+                sum=i;
             }else{
-                rangeSum+= weights[i];
+                sum+=i;
+            }
+            // System.out.println("mid: "+mid+" sum: "+(sum)+" => "+totalDaysUsed);
+            if(totalDaysUsed>days){
+                return false;
             }
         }
-        return daysCount<=days;
+
+        return totalDaysUsed<=days;
+    }
+
+    public int[] sumOfArray(int[] weights){
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        for(int i: weights){
+            sum+=i;
+            max=Math.max(max,i);
+        }
+
+        return new int[]{max,sum};
     }
 }
