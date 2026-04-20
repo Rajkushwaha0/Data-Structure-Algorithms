35f9d59cc30289dfaff7f887bd29c0b3419b0794
Raj kushwaha <87632065+Rajkushwaha0@users.noreply.github.com>
2026-04-21 02:03:50 +0530

    Update LC-E-1539-Kth Missing Positive Number.java

--- Learn_Binary Search/LC-E-1539-Kth Missing Positive Number.java
+++ Learn_Binary Search/LC-E-1539-Kth Missing Positive Number.java
@@ -1,3 +1,30 @@
+//O(LOG(N))
+class Solution {
+    public int findKthPositive(int[] arr, int k) {
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+
+        while(s<=e){
+            int mid = s + (e-s)/2;
+
+            int missing = arr[mid]-(mid+1);
+
+            if(missing>=k){
+                e=mid-1;
+            }else{
+                s=mid+1;
+            }
+        }
+
+        return s+k; //e+1+k you know where this stopped s must be ahead of e and 
+        // arr[e]+ more (k-missing)
+        //arr[e]+ (k-(arr[e]-(e+1)))
+        //arr[e]+k-arr[e]+e+1
+        //k+e+1
+    }
+}
+
 class Solution {
     public int findKthPositive(int[] arr, int k) {
         //BF
